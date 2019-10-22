acdecda6b90771ee692ba65d5121f3f4d264c8bb
wissem Khayati <khayati.wissem@gmail.com>
2019-10-22 01:00:12 +0100

    Add files via upload

---
+++ FileLoader.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.Video;
+
+
+
+
+public class FileLoader : MonoBehaviour
+{
+
+    public string fileName;
+    public string url;
+    
+    public Texture2D Source_texture;
+    public string Source_video;
+
+    public FileLoader(string fileName, string url){
+        this.fileName = fileName;
+        this.url = url;
+    }
+
+    public IEnumerator LoadImage() {
+        
+        string filePath =  Application.streamingAssetsPath+"/"+fileName;
+        Texture2D myTexture = null;
+        byte[] fileData;
+ 
+        //Check if file image is available in local storage
+        if ( File.Exists(filePath) )
+        {
+            fileData = File.ReadAllBytes(filePath);
+            myTexture = new Texture2D(2, 2);
+            myTexture.LoadImage(fileData);
+        }else{
+
+            //Download file image from external url
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+
+            if(www.isNetworkError || www.isHttpError) {
+            
+            }
+            else {
+
+                myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+               
+
+                //save in storage
+                File.WriteAllBytes(Application.streamingAssetsPath+"/"+fileName, myTexture.EncodeToPNG());
+            }
+        }
+
+        Source_texture = myTexture;
+        yield return null;
+    }
+
+
+    public IEnumerator LoadVideo() {
+
+        
+        //load in storage
+        string filePath =  Application.streamingAssetsPath+"/"+fileName+".mp4";
+
+        if ( File.Exists(filePath) )
+        {
+           
+            Source_video = filePath;
+        }else{
+            UnityWebRequest _videoRequest = UnityWebRequest.Get (url);
+
+            yield return _videoRequest.SendWebRequest();
+
+            if (_videoRequest.isDone == false || _videoRequest.error != null)
+            {   Debug.Log ("Request = " + _videoRequest.error );}
+
+
+            byte[] _videoBytes = _videoRequest.downloadHandler.data;
+
+    
+            File.WriteAllBytes (filePath, _videoBytes);
+            Debug.Log (filePath);
+
+            Source_video = filePath;
+            yield return null;
+        }
+    }
+}
---
+++ test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class test : MonoBehaviour
+{
+
+    public RawImage image;
+    public VideoPlayer video;
+
+    IEnumerator Start()
+    {   
+        
+        FileLoader loader1 = new FileLoader("downloadedImage", "http://vps587231.ovh.net/static/cocacola.png");
+        yield return StartCoroutine(loader1.LoadImage());
+        image.texture = loader1.Source_texture;
+
+        
+        FileLoader loader2 = new FileLoader("downloadedVideo", "http://vps587231.ovh.net/static/TVPub/Stories/cocacola1.mp4");
+        yield return StartCoroutine(loader2.LoadVideo());
+        video.url = loader2.Source_video;
+    }
+
+    void Update()
+    {
+        
+    }
+}
