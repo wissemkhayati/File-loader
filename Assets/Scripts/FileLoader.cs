3e22fb70deef24b3550ec6331d8cf1ff4f5a1047
wissem Khayati <khayati.wissem@gmail.com>
2019-10-22 11:42:00 +0100

    Update FileLoader.cs

--- Assets/Scripts/FileLoader.cs
+++ Assets/Scripts/FileLoader.cs
@@ -4,83 +4,84 @@ using System.IO;
 using UnityEngine.Networking;
 
 
-public class FileLoader : MonoBehaviour
-{
+public class FileLoader: MonoBehaviour {
 
-    public string fileName;
-    public string url;
-    
-    public Texture2D Source_texture;
-    public string Source_video;
+ public string fileName;
+ public string url;
 
-    public FileLoader(string fileName, string url){
-        this.fileName = fileName;
-        this.url = url;
-    }
+ public Texture2D Source_texture;
+ public string Source_video;
 
-    public IEnumerator LoadImage() {
-        
-        string filePath =  Application.streamingAssetsPath+"/"+fileName;
-        Texture2D myTexture = null;
-        byte[] fileData;
- 
-        //Check if file image is available in local storage
-        if ( File.Exists(filePath) )
-        {
-            fileData = File.ReadAllBytes(filePath);
-            myTexture = new Texture2D(2, 2);
-            myTexture.LoadImage(fileData);
-        }else{
+ public FileLoader(string fileName, string url) {
+  this.fileName = fileName;
+  this.url = url;
+ }
 
-            //Download file image from external url
-            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
-            yield return www.SendWebRequest();
+ public IEnumerator LoadImage() {
 
-            if(www.isNetworkError || www.isHttpError) {
-            
-            }
-            else {
+  string filePath = Application.streamingAssetsPath + "/" + fileName;
+  Texture2D myTexture = null;
+  byte[] fileData;
 
-                myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-               
+  //Check if file image is available in local storage
+  if (File.Exists(filePath)) {
+   fileData = File.ReadAllBytes(filePath);
+   myTexture = new Texture2D(2, 2);
+   myTexture.LoadImage(fileData);
+  } else {
 
-                //save in storage
-                File.WriteAllBytes(Application.streamingAssetsPath+"/"+fileName, myTexture.EncodeToPNG());
-            }
-        }
+   //Download file image from external url
+   UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+   yield
+   return www.SendWebRequest();
 
-        Source_texture = myTexture;
-        yield return null;
-    }
+   if (www.isNetworkError || www.isHttpError) {
+
+   } else {
+
+    myTexture = ((DownloadHandlerTexture) www.downloadHandler).texture;
 
 
-    public IEnumerator LoadVideo() {
+    //save in storage
+    File.WriteAllBytes(Application.streamingAssetsPath + "/" + fileName, myTexture.EncodeToPNG());
+   }
+  }
 
-        
-        //load in storage
-        string filePath =  Application.streamingAssetsPath+"/"+fileName+".mp4";
-
-        if ( File.Exists(filePath) )
-        {
-           
-            Source_video = filePath;
-        }else{
-            UnityWebRequest _videoRequest = UnityWebRequest.Get (url);
-
-            yield return _videoRequest.SendWebRequest();
-
-            if (_videoRequest.isDone == false || _videoRequest.error != null)
-            {   Debug.Log ("Request = " + _videoRequest.error );}
+  Source_texture = myTexture;
+  yield
+  return null;
+ }
 
 
-            byte[] _videoBytes = _videoRequest.downloadHandler.data;
+ public IEnumerator LoadVideo() {
 
-    
-            File.WriteAllBytes (filePath, _videoBytes);
-            Debug.Log (filePath);
 
-            Source_video = filePath;
-            yield return null;
-        }
-    }
+  //load in storage
+  string filePath = Application.streamingAssetsPath + "/" + fileName + ".mp4";
+
+  if (File.Exists(filePath)) {
+
+   Source_video = filePath;
+  } else {
+   UnityWebRequest _videoRequest = UnityWebRequest.Get(url);
+
+   yield
+   return _videoRequest.SendWebRequest();
+
+   if (_videoRequest.isDone == false || _videoRequest.error != null) {
+    Debug.Log("Request = " + _videoRequest.error);
+   }
+
+
+   byte[] _videoBytes = _videoRequest.downloadHandler.data;
+
+
+   File.WriteAllBytes(filePath, _videoBytes);
+   Debug.Log(filePath);
+
+   Source_video = filePath;
+   yield
+   return null;
+  }
+ }
 }
