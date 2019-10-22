93c063ac735f8b70249421105dcbd051cbe31ec2
wissem Khayati <khayati.wissem@gmail.com>
2019-10-22 01:02:07 +0100

    Update and rename test.cs to Test.cs

--- Test.cs
+++ Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
-public class test : MonoBehaviour
+public class Test : MonoBehaviour
 {
 
     public RawImage image;
@@ -12,19 +12,15 @@ public class test : MonoBehaviour
 
     IEnumerator Start()
     {   
-        
+        //Load, save and display image from an external url
         FileLoader loader1 = new FileLoader("downloadedImage", "http://vps587231.ovh.net/static/cocacola.png");
         yield return StartCoroutine(loader1.LoadImage());
         image.texture = loader1.Source_texture;
 
-        
+        //Load, save and play video from an external url
         FileLoader loader2 = new FileLoader("downloadedVideo", "http://vps587231.ovh.net/static/TVPub/Stories/cocacola1.mp4");
         yield return StartCoroutine(loader2.LoadVideo());
         video.url = loader2.Source_video;
     }
 
-    void Update()
-    {
-        
-    }
 }
